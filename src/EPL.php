c7adb97098874633cf90fe97aa848a2706021353
Michael Fernandes Rodrigues <michael.rodrigues@epl.gov.br>
2015-01-17 12:05:55 -0500

    Testst

---
+++ src/EPL.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of EPL
+ *
+ * @author Michael Fernandes <michael.rodrigues@epl.gov.br>
+ */
+class EPL
+{
+
+    /**
+     * @param integer $first
+     * @param integer $second
+     * @return integer
+     */
+    public function sum ($first, $second)
+    {
+        return $first + $second;
+    }
+
+    /**
+     * @param mixed $var
+     * @return boolean
+     */
+    public function isEmpty ($var)
+    {
+        return empty($var);
+    }
+
+    /**
+     * @return boolean
+     */
+    private function stopStop ()
+    {
+        return false;
+    }
+
+    /**
+     * 
+     */
+    private function UPPPERCASE__________GIGAGIGAGIGAGIGAGIGAGIGA______2121212121212121212______PROBLEM ()
+    {
+        'nao faz nadaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+        'soh ocupaaaaaaaaaaaaaaaaaaaaa espacoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo';
+    }
+
+    /**
+     * 
+     */
+    public function muitoComplexoMtRuim ()
+    {
+        if (1) {
+        if (1 !== 1) {
+                print $this->isEmpty($this->stopStop());
+            } else {
+                for ($i = 1; $i <= 1000; $i++) {
+                    switch (155) {
+                        case 1:
+                            print $this->sum(1, '2');
+                            break;
+
+                        case 115:
+                            if ($this->sum(1, 1) === 3) {
+                                print 'nadaaaa';
+                            }
+
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+}
